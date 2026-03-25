a4f691446024fb016df680594b2f4434a54d0391
Ibrahem Jasim Alsalih <alazawy.m91@gmail.com>
2026-03-25 13:26:40 +0100

    Initial Commit

---
+++ Filmdatabasen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filmdatabasen
+//
+//  Created by Ibrahim Jasim Alsalih on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filmdatabasen/FilmdatabasenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmdatabasenApp.swift
+//  Filmdatabasen
+//
+//  Created by Ibrahim Jasim Alsalih on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct FilmdatabasenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
